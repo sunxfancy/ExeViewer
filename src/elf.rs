c47cce4e4ee4c6c5a474f0f4d19495cb05c79660
sunxfancy <sunxfancy@gmail.com>
2024-10-30 16:27:57 -0700

    first version

--- src/elf.rs
+++ src/elf.rs
@@ -4,7 +4,8 @@ use elf::endian::AnyEndian;
 use elf::note::Note;
 use elf::note::NoteGnuBuildId;
 use elf::section::SectionHeader;
-use std::path::PathBuf;
+use ratatui::widgets::canvas::Shape;
+use iced_x86::{Decoder, DecoderOptions, Formatter, Instruction, NasmFormatter};
 
 pub struct Elf<'a> {
     file_data: &'a Vec<u8>,
@@ -20,4 +21,40 @@ impl<'a> Elf<'a> {
             elf,        // 传递给 ElfBytes 的引用在生命周期上与结构体匹配
         }
     }
+
+    pub fn decompile_symbol(&self, symbol_address: u64, symbol_size: usize) -> String {
+        // 读取内存片段
+        let shdr = self.elf.section_header_by_name(".text").expect("Section not found");
+        let (section, header) = self.elf.section_data(&shdr.unwrap()).expect("Section data not found");
+        
+        if (symbol_address < shdr.unwrap().sh_addr)  {
+            return String::from("Symbol out of range");
+        }
+
+        let code_offset = (symbol_address - shdr.unwrap().sh_addr) as usize;
+
+        if (code_offset + symbol_size) > shdr.unwrap().sh_size as usize {
+            return String::from("Symbol out of range");
+        }
+
+        let code = &section[code_offset..code_offset + symbol_size];
+    
+        let mut decoder = Decoder::with_ip(64, code, symbol_address, DecoderOptions::NONE);
+        let mut formatter = iced_x86::IntelFormatter::new();
+    
+        let mut instruction = Instruction::default();
+        let mut buffer = String::new();
+        while decoder.can_decode() {
+            decoder.decode_out(&mut instruction);
+            
+            let mut output = String::new();
+            formatter.format(&instruction, &mut output);
+            
+            buffer.push_str(output.as_str());
+            buffer.push('\n');
+        }
+        buffer
+    }
+    
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
+use clap::Parser;
 use std::fs;
 use std::io::{self, stdout};
 use std::path::PathBuf;
-use clap::Parser;
 
 use ratatui::layout::{Constraint, Direction, Layout};
+use ratatui::prelude::Backend;
 use ratatui::style::{Modifier, Style};
 use ratatui::widgets::ListState;
 use ratatui::{
     backend::CrosstermBackend,
     crossterm::{
         event::{self, Event, KeyCode},
-        terminal::{
-            disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
-        },
+        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
         ExecutableCommand,
     },
-    widgets::{Block, Paragraph, List, ListDirection},
+    widgets::{Block, List, ListDirection, Paragraph},
     Frame, Terminal,
 };
 
@@ -30,75 +29,152 @@ struct Args {
     file: PathBuf,
 }
 
+struct App<'a> {
+    should_quit: bool,
+    elf_file: Elf<'a>,
+    symbol_page: SymbolPage<'a>,
+}
+
+trait Page {
+    fn draw(&mut self, frame: &mut Frame) -> ();
+}
+
+struct SymbolPage<'a> {
+    content: Vec<Symbol>,
+    list: List<'a>,
+    state: ListState,
+}
+
+struct Symbol {
+    address: u64,
+    size: u64,
+    decompiled: bool,
+    data: String,
+}
+
+impl App<'_> {
+    fn new<'a>(elf_file: Elf<'a>, name_list: Vec<&'a str>, content: Vec<Symbol>) -> App<'a> {
+        let list = List::new(name_list)
+            .block(Block::bordered().title("Symbols"))
+            .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
+            .highlight_symbol(">>")
+            .repeat_highlight_symbol(true)
+            .direction(ListDirection::TopToBottom);
+        App {
+            should_quit: false,
+            elf_file: elf_file,
+            symbol_page: SymbolPage {
+                content: content,
+                list: list,
+                state: ListState::default(),
+            },
+        }
+    }
+
+    fn run<B: Backend>(mut self, mut terminal: Terminal<B>) -> Result<(), io::Error> {
+        while !self.should_quit {
+            terminal.draw(|frame| {
+                self.symbol_page.draw(frame);
+            })?;
+            self.should_quit = self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn handle_events(&mut self) -> Result<bool, io::Error> {
+        if event::poll(std::time::Duration::from_millis(50))? {
+            if let Event::Key(key) = event::read()? {
+                if key.kind == event::KeyEventKind::Press {
+                    match key.code {
+                        KeyCode::Char('q') => return Ok(true),
+                        KeyCode::Down => {
+                            self.select_next();
+                        }
+                        KeyCode::Up => {
+                            self.select_previous();
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+        Ok(false)
+    }
+
+    fn load_symbol(&mut self, idx: usize) {
+        let symbol = &self.symbol_page.content[idx];
+        if !symbol.decompiled {
+            let decompiled = self
+                .elf_file
+                .decompile_symbol(symbol.address, symbol.size as usize);
+            self.symbol_page.content[idx].data = decompiled;
+            self.symbol_page.content[idx].decompiled = true;
+        }
+    }
+
+    fn select_next(&mut self) {
+        self.symbol_page.state.select_next();
+        let idx = self.symbol_page.state.selected().unwrap();
+        self.load_symbol(idx);
+    }
+
+    fn select_previous(&mut self) {
+        self.symbol_page.state.select_previous();
+    }
+}
+
+impl Page for SymbolPage<'_> {
+    fn draw(&mut self, frame: &mut Frame) -> () {
+        let layout = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(vec![Constraint::Percentage(30), Constraint::Percentage(70)])
+            .split(frame.area());
+
+        frame.render_stateful_widget(&self.list, layout[0], &mut self.state);
+
+        let selected = self.state.selected();
+        frame.render_widget(
+            Paragraph::new(if selected.is_none() {
+                "Select a symbol to decompile"
+            } else {
+                self.content[selected.unwrap()].data.as_str()
+            })
+            .block(Block::default().title("Assembly")),
+            layout[1],
+        )
+    }
+}
 
 fn main() -> io::Result<()> {
     let args = Args::parse();
     let buffer = fs::read(&args.file).expect("file should read");
-    let file = Elf::new(&buffer);
-    
+    let file: Elf<'_> = Elf::new(&buffer);
+
     // Find lazy-parsing types for the common ELF sections (we want .dynsym, .dynstr, .hash)
     let symtable = file.elf.symbol_table().expect("symtab should parse");
     let (symtab, strtab) = symtable.unwrap();
 
-    let mut content = Vec::new();
+    let mut name_list: Vec<&str> = Vec::new();
+    let mut content: Vec<Symbol> = Vec::new();
     symtab.iter().for_each(|sym| {
-        content.push(strtab.get(sym.st_name as usize).unwrap());
+        let name = strtab.get(sym.st_name as usize).unwrap();
+        name_list.push(name);
+        content.push(Symbol {
+            address: sym.st_value,
+            size: sym.st_size,
+            decompiled: false,
+            data: String::new(),
+        });
     });
 
-    let list = List::new(content)
-        .block(Block::bordered().title("Symbols"))
-        .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
-        .highlight_symbol(">>")
-        .repeat_highlight_symbol(true)
-        .direction(ListDirection::TopToBottom);
-    let mut state = ListState::default();
+    let app = App::new(file, name_list, content);
 
     enable_raw_mode()?;
     stdout().execute(EnterAlternateScreen)?;
-    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
-
-    let mut should_quit = false;
-    while !should_quit {
-        terminal.draw(|frame: &mut Frame|{
-            // ui(&mut frame);
-            ui(frame, &args, &list, &mut state);
-        })?;
-        should_quit = handle_events()?;
-    }
+    let terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    app.run(terminal)?;
 
     disable_raw_mode()?;
     stdout().execute(LeaveAlternateScreen)?;
     Ok(())
-}
-
-fn handle_events() -> io::Result<bool> {
-    if event::poll(std::time::Duration::from_millis(50))? {
-        if let Event::Key(key) = event::read()? {
-            if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(true);
-            }
-        }
-    }
-    Ok(false)
-}
-
-fn ui(frame: &mut Frame, args: &Args, list: &List, state: &mut ListState) {
-    let layout = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints(vec![
-            Constraint::Percentage(30),
-            Constraint::Percentage(70),
-        ])
-        .split(frame.area());
-
-    frame.render_stateful_widget(
-        list,
-        layout[0],
-        state,
-    );
-
-    frame.render_widget(
-        Paragraph::new(args.file.to_str().unwrap()).block(Block::default().title("File")),
-        layout[1],
-    )
 }
